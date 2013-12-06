f3ba1b830697f925a0d40e2844d27d7caf6e977b
aelgru <aelgru@gmail.com>
2013-12-07 00:04:57 +0100

    v0.0.2

--- build.py
+++ build.py
@@ -32,7 +32,7 @@ description = 'Please visit {0} for more information!'.format(url)
 authors = [Author('Michael Gruber', 'aelgru@gmail.com')]
 license = 'Apache License, Version 2.0'
 summary = 'A fluent interface facade for Michael Foord`s Mock.'
-version = '0.0.1'
+version = '0.0.2'
 
 default_task = ['analyze', 'publish']
 
