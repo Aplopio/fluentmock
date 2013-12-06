df808050c18e90c07c2b203324877f6aa8775db0
aelgru <aelgru@gmail.com>
2013-12-06 22:40:20 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+# fluentmock
+
+A fluent interface facade for Michael Foord's Mock.
+
+```python
+import somemodule
+
+when(somemodule).functionname().then_return(3)
+```
+
+## License
+
+fluentmock is licensed under the
+[Apache License, Version 2.0](https://raw.github.com/aelgru/committer/master/src/main/python/committer/LICENSE.txt)
---
+++ build.py
@@ -0,0 +1,71 @@
+#   fluentmock
+#   Copyright 2012-2013 Michael Gruber
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from pybuilder.core import Author, init, use_plugin
+
+use_plugin('python.core')
+
+use_plugin('copy_resources')
+use_plugin('filter_resources')
+
+use_plugin('python.coverage')
+use_plugin('python.distutils')
+use_plugin('python.flake8')
+use_plugin('python.install_dependencies')
+use_plugin('python.unittest')
+
+url = 'https://github.com/aelgru/fluentmock'
+description = 'Please visit {0} for more information!'.format(url)
+
+authors = [Author('Michael Gruber', 'aelgru@gmail.com')]
+license = 'Apache License, Version 2.0'
+summary = 'A fluent interface facade for Michael Foord`s Mock.'
+version = '0.0.1'
+
+default_task = ['analyze', 'publish']
+
+
+@init
+def set_properties(project):
+    project.build_depends_on('coverage')
+    project.build_depends_on('mock')
+    project.build_depends_on('wheel')
+
+    project.set_property('dir_source_integrationtest_python', 'src/unittest')
+
+    project.set_property('coverage_break_build', True)
+    project.get_property("coverage_exceptions").append('targetpackage')
+
+    project.set_property('copy_resources_target', '$dir_dist')
+    project.get_property('copy_resources_glob').append('LICENSE.txt')
+
+    project.include_file('fluentmock', 'LICENSE.txt')
+
+    project.get_property('filter_resources_glob').append('**/fluentmock/__init__.py')
+
+    project.set_property('flake8_verbose_output', True)
+    project.set_property('flake8_break_build', True)
+    project.set_property('flake8_include_test_sources', True)
+
+    project.get_property('distutils_commands').append('bdist_wheel')
+    project.set_property('distutils_classifiers', [
+        'Development Status :: 5 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Utilities'])
