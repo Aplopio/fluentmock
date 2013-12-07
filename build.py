4dee925bf50f168da6bac64160f5f710317e8d1a
aelgru <aelgru@gmail.com>
2013-12-07 18:22:35 +0100

    v0.0.5

--- build.py
+++ build.py
@@ -32,7 +32,7 @@ description = 'Please visit {0} for more information!'.format(url)
 authors = [Author('Michael Gruber', 'aelgru@gmail.com')]
 license = 'Apache License, Version 2.0'
 summary = 'Mocking framework with a fluent interface.'
-version = '0.0.4'
+version = '0.0.5'
 
 default_task = ['analyze', 'publish']
 
