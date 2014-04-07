649a3adb49430858e4a190b10bec13415a301a55
aelgru <aelgru@gmail.com>
2014-04-07 18:48:39 +0200

    v0.2.8

---
+++ release.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from subprocess import PIPE, check_call
+from sys import argv
+
+version = argv[1]
+print "Releasing version {version}".format(version=version)
+
+check_call(["ci", "v{version}".format(version=version)])
+check_call(["git", "tag", "v{version}".format(version=version)])
+check_call(["git", "push", "--tags"])
+check_call(["./setup.py", "sdist", "upload"], cwd="target/dist/fluentmock-{version}".format(version=version))
+check_call(["./setup.py", "bdist_wheel", "upload"], cwd="target/dist/fluentmock-{version}".format(version=version))
