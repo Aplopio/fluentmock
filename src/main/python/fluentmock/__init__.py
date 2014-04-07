5243f0f14f0c0d9ed2da92ef7b677e339c655564
aelgru <aelgru@gmail.com>
2014-04-07 18:34:33 +0200

    Removing unused block

--- src/main/python/fluentmock/__init__.py
+++ src/main/python/fluentmock/__init__.py
@@ -345,12 +345,6 @@ class Verifier(FluentTarget):
         self._ensure_valid_usage_of_any_arguments(arguments)
         count_of_matching_calls = self._count_matching_call_entries(arguments, keyword_arguments)
 
-        if self._matcher is AT_LEAST_ONCE and not AT_LEAST_ONCE.matches(count_of_matching_calls):
-
-            method_of_mock = getattr(self.object, self.attribute_name)
-            if isinstance(self.object, Mock) and isinstance(method_of_mock, Mock):
-                method_of_mock.assert_called_with(*arguments, **keyword_arguments)
-
         if not self._matcher.matches(count_of_matching_calls):
             expected_call_entry = FluentCallEntry(self.object, self.attribute_name, arguments, keyword_arguments)
 
