0bb9b723f8c12caa1746c6ed69f5ad1b324a2647
aelgru <aelgru@gmail.com>
2014-06-28 17:56:47 +0200

    Using pybuilder_header_plugin to ensure file headers contain license

--- build.py
+++ build.py
@@ -27,6 +27,7 @@ use_plugin('python.install_dependencies')
 use_plugin('python.unittest')
 
 use_plugin('pypi:pybuilder_release_plugin')
+use_plugin('pypi:pybuilder_header_plugin')
 
 url = 'https://github.com/aelgru/fluentmock'
 description = "Please visit {url}".format(url=url)
@@ -36,7 +37,7 @@ license = 'Apache License, Version 2.0'
 summary = "Fluent interface facade for Michael Foord's mock."
 version = '0.2.13'
 
-default_task = ['analyze', 'publish']
+default_task = ['analyze', 'check_source_file_headers', 'publish']
 
 
 @init
@@ -74,3 +75,20 @@ def set_properties(project):
         'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Testing',
         'Topic :: Software Development :: Quality Assurance'])
+
+    project.set_property('pybuilder_header_plugin_break_build', True)
+    project.set_property('pybuilder_header_plugin_expected_header', """#   fluentmock
+#   Copyright 2013-2014 Michael Gruber
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+""")
